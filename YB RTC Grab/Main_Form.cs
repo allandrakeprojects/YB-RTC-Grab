2aaf889052b7a9537430c6c86e29be728e0e7b41
SSIMAKATI\adulay <allandulay69@gmail.com>
2018-11-15 11:20:53 +0800

    Initial commit

---
+++ YB RTC Grab/Main_Form.cs
@@ -0,0 +1,238 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YB_RTC_Grab
+{
+    public partial class Main_Form : Form
+    {
+        private bool m_aeroEnabled;
+        private bool __isClose;
+        private int __secho;
+        private int __display_length = 20;
+        private int __result_count_json;
+        private int __total_page;
+        private JObject __jo;
+        private ChromiumWebBrowser chromeBrowser;
+
+        // Drag Header to Move
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        // ----- Drag Header to Move
+
+        // Form Shadow
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+        private const int CS_DROPSHADOW = 0x00020000;
+        private const int WM_NCPAINT = 0x0085;
+        private const int WM_ACTIVATEAPP = 0x001C;
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+        private const int WS_MINIMIZEBOX = 0x20000;
+        private const int CS_DBLCLKS = 0x8;
+        public struct MARGINS
+        {
+            public int leftWidth;
+            public int rightWidth;
+            public int topHeight;
+            public int bottomHeight;
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                m_aeroEnabled = CheckAeroEnabled();
+
+                CreateParams cp = base.CreateParams;
+                if (!m_aeroEnabled)
+                    cp.ClassStyle |= CS_DROPSHADOW;
+
+                cp.Style |= WS_MINIMIZEBOX;
+                cp.ClassStyle |= CS_DBLCLKS;
+                return cp;
+            }
+        }
+        private bool CheckAeroEnabled()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                int enabled = 0;
+                DwmIsCompositionEnabled(ref enabled);
+                return (enabled == 1) ? true : false;
+            }
+            return false;
+        }
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCPAINT:
+                    if (m_aeroEnabled)
+                    {
+                        var v = 2;
+                        DwmSetWindowAttribute(Handle, 2, ref v, 4);
+                        MARGINS margins = new MARGINS()
+                        {
+                            bottomHeight = 1,
+                            leftWidth = 0,
+                            rightWidth = 0,
+                            topHeight = 0
+                        };
+                        DwmExtendFrameIntoClientArea(Handle, ref margins);
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT)
+                m.Result = (IntPtr)HTCAPTION;
+        }
+        // ----- Form Shadow
+
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        // Drag Header to Move
+        private void panel_header_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        private void label_title_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        private void panel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        // ----- Drag Header to Move
+
+        // Click Close
+        private void pictureBox_close_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Exit the program?", "YB", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                __isClose = true;
+                Environment.Exit(0);
+            }
+        }
+
+        // Click Minimize
+        private void pictureBox_minimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        // Form Closing
+        private void Main_Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!__isClose)
+            {
+                DialogResult dr = MessageBox.Show("Exit the program?", "YB", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+            Environment.Exit(0);
+        }
+
+        // Form Load
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            InitializeChromium();
+        }
+
+        // CefSharp Initialize
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+
+            settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF";
+            Cef.Initialize(settings);
+            chromeBrowser = new ChromiumWebBrowser("http://103.4.104.8/page/manager/login.jsp");
+            panel_cefsharp.Controls.Add(chromeBrowser);
+            chromeBrowser.AddressChanged += ChromiumBrowserAddressChanged;
+        }
+
+        // CefSharp Address Changed
+        private void ChromiumBrowserAddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            if (e.Address.ToString().Equals("http://103.4.104.8/page/manager/login.jsp"))
+            {
+                Invoke(new Action(() =>
+                {
+                    chromeBrowser.FrameLoadEnd += (sender_, args) =>
+                    {
+                        if (args.Frame.IsMain)
+                        {
+                            args.Frame.ExecuteJavaScriptAsync("document.getElementById('username').value = 'testrain';");
+                            args.Frame.ExecuteJavaScriptAsync("document.getElementById('password').value = 'rain12345';");
+                        }
+                    };
+                }));
+            }
+
+            if (e.Address.ToString().Equals("http://103.4.104.8/page/manager/member/search.jsp"))
+            {
+                Invoke(new Action(() =>
+                {
+                    panel_cefsharp.Visible = false;
+                    label_brand.Visible = true;
+                    pictureBox_loader.Visible = true;
+                    //___GetPlayerLists();
+                }));
+            }
+        }
+    }
+}
